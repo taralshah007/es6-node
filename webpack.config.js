b5f176e35b795bed01ddfb6a62c3165a11e617c9
taral shah <tashah@ebay.com>
2017-05-15 12:48:21 -0700

    Adding babel configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-    entry: ['./src/index.js'],
+    entry: ['./src/index.js','./src/es6-let/index.js'],
     output: {
         path: __dirname + '/build',
         filename: 'main.js',
         publicPath:'/build'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
     },
     devServer: {
         port:3001,
