cce427b4668a1e783f983e4f029aa41962d614bc
taral shah <tashah@ebay.com>
2017-05-16 12:06:44 -0700

    More changes.

--- src/es6-let/index.js
+++ src/es6-let/index.js
@@ -1,12 +1,12 @@
-document.write("<br/>");
 
 let x = 15;
 var y = 10;
-document.write('ES6: Example of let ');
-document.write(x);
+
+document.write('<br/> Using document.write is not recommended. This is for example purpose only. <br/> ES6: Example of let '+x);
 
 {
     document.write("<br/>" + "changing the value of x in a block using let x =30");
+    console.log("changing the value of x in a block using let x =30");
     let x = 30;
     document.write("<br/>" + x);
 }
